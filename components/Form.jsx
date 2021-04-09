8435de20ea8582d8517b76c381179abd94fb3295
aiibe <souksavanh.sy@gmail.com>
2021-04-09 17:05:22 +0200

    minor changes

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,51 +1,53 @@
-import { useState } from 'react'
+import { useState } from "react"
 
 export default function Form({ onSubmit }) {
-	const initialState = Object.freeze({ name: '', paid: '' })
-	const [state, setstate] = useState(initialState)
+  const initialState = Object.freeze({ name: "", paid: "" })
+  const [state, setstate] = useState(initialState)
 
-	function handleSubmit(event) {
-		event.preventDefault()
-		onSubmit(state)
-		setstate(initialState)
-	}
+  function handleSubmit(event) {
+    event.preventDefault()
+    onSubmit(state)
+    setstate(initialState)
+  }
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<div className='flex mb-2'>
-				<div className='w-1/2 mr-2'>
-					<label className='block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2'>
-						Name
-					</label>
-					<input
-						onChange={(event) => setstate({ ...state, name: event.target.value })}
-						className='appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-2 px-2 mb-3'
-						type='text'
-						placeholder='Jane'
-						value={state.name}
-						required={true}
-					/>
-				</div>
-				<div className='w-1/2 ml-2'>
-					<label className='block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2'>
-						$$$
-					</label>
-					<input
-						required={true}
-						onChange={(event) => setstate({ ...state, paid: event.target.value })}
-						className='appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-2 px-2'
-						type='text'
-						placeholder='33.1'
-						value={state.paid}
-					/>
-				</div>
-			</div>
-			<button
-				type='submit'
-				className='mb-4 w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700'
-			>
-				Add Expense
-			</button>
-		</form>
-	)
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="flex mb-2">
+        <div className="w-1/2 mr-2">
+          <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+            Name
+          </label>
+          <input
+            className="focus:outline-none focus:ring focus:border-blue-300"
+            onChange={event => setstate({ ...state, name: event.target.value })}
+            className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-2 px-2 mb-3"
+            type="text"
+            placeholder="Jane"
+            value={state.name}
+            required={true}
+          />
+        </div>
+        <div className="w-1/2 ml-2">
+          <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+            $$$
+          </label>
+          <input
+            className="focus:outline-none focus:ring focus:border-blue-300"
+            required={true}
+            onChange={event => setstate({ ...state, paid: event.target.value })}
+            className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-2 px-2"
+            type="text"
+            placeholder="33.1"
+            value={state.paid}
+          />
+        </div>
+      </div>
+      <button
+        type="submit"
+        className="mb-4 w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+      >
+        Add Expense
+      </button>
+    </form>
+  )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,89 +1,94 @@
-import Header from '../components/Header'
-import Top from '../components/Top'
-import Form from '../components/Form'
-import { useState, useMemo } from 'react'
+import Header from "../components/Header"
+import Top from "../components/Top"
+import Form from "../components/Form"
+import { useState, useMemo } from "react"
 
 export default function Home() {
-	const [currency, setCurrency] = useState('$')
-	const [record, setrecord] = useState([])
-	const derivedRecord = useMemo(() => getDerivedRecords(record), [record])
+  const [currency, setCurrency] = useState("$")
+  const [record, setrecord] = useState([])
+  const derivedRecord = useMemo(() => getDerivedRecords(record), [record])
 
-	function getDerivedRecords(record) {
-		return record.reduce((acc, { name, paid }) => {
-			if (acc.length === 0) {
-				acc.push({ name, records: [paid] })
-			} else {
-				const members = [...new Set(acc.map((a) => a.name))]
-				if (!members.includes(name)) {
-					acc.push({ name, records: [paid] })
-				} else {
-					const slot = acc.findIndex((s) => s.name === name)
-					const newItem = { ...acc[slot], records: [...acc[slot].records, paid] }
-					acc[slot] = newItem
-				}
-			}
-			return acc
-		}, [])
-	}
+  function getDerivedRecords(record) {
+    return record.reduce((acc, { name, paid }) => {
+      if (acc.length === 0) {
+        acc.push({ name, records: [paid] })
+      } else {
+        const members = [...new Set(acc.map(a => a.name))]
+        if (!members.includes(name)) {
+          acc.push({ name, records: [paid] })
+        } else {
+          const slot = acc.findIndex(s => s.name === name)
+          const newItem = {
+            ...acc[slot],
+            records: [...acc[slot].records, paid],
+          }
+          acc[slot] = newItem
+        }
+      }
+      return acc
+    }, [])
+  }
 
-	const getMembers = () => {
-		const members = [...new Set(record.map((r) => r.name))] // ["alice", "paul"]
-		return members
-	}
+  const getMembers = () => {
+    const members = [...new Set(record.map(r => r.name))] // ["alice", "paul"]
+    return members
+  }
 
-	const handleSubmit = ({ name, paid }) => {
-		const rgx = new RegExp('^-?\\d*(\\.\\d+)?$') // Only integers and floats (comma is falsy)
-		const id = Math.random().toString(36).substring(7) // Generate random short string
-		if (!rgx.test(paid)) paid = 0
-		setrecord([
-			...record,
-			{ name: name.toLowerCase().trim(), paid: parseFloat(paid), id },
-		])
-	}
+  const handleSubmit = ({ name, paid }) => {
+    const rgx = new RegExp("^-?\\d*(\\.\\d+)?$") // Only integers and floats (comma is falsy)
+    const id = Math.random().toString(36).substring(7) // Generate random short string
+    if (!rgx.test(paid)) paid = 0
+    setrecord([
+      ...record,
+      { name: name.toLowerCase().trim(), paid: parseFloat(paid), id },
+    ])
+  }
 
-	return (
-		<div className='w-full max-w-2xl mx-auto px-4'>
-			<Header />
+  return (
+    <div className="w-full max-w-2xl mx-auto px-4">
+      <Header />
 
-			<Top record={record} currency={currency} />
+      <Top record={record} currency={currency} />
 
-			{derivedRecord.map(({ name, records }) => (
-				<div
-					className='bg-white shadow overflow-hidden mb-4 sm:rounded-lg'
-					key={name}
-				>
-					<div className='px-3 py-3'>
-						<div className='flex justify-between items-center'>
-							<div className='flex'>
-								<div className='flex-shrink-0 h-12 w-12 mr-3'>
-									<img
-										className='block rounded-full mr-4'
-										src={`https://api.multiavatar.com/${name}.png`}
-										alt={name}
-									/>
-								</div>
-								<div>
-									<h3 className='text-base font-bold capitalize'>{name}</h3>
-									<ul>
-										{records.map((paid) => (
-											<li key={paid} className='text-sm text-gray-500'>
-												→ paid {currency}
-												{paid}
-											</li>
-										))}
-									</ul>
-								</div>
-							</div>
-							<h2 className='text-2xl font-bold'>
-								<span className='text-sm mr-1 align-top'>$</span>
-								{!records.length ? 0 : records.reduce((t, paid) => (t += paid), 0)}
-							</h2>
-						</div>
-					</div>
-				</div>
-			))}
+      {derivedRecord.map(({ name, records }) => (
+        <div
+          className="bg-white shadow overflow-hidden mb-4 sm:rounded-lg"
+          key={name}
+        >
+          <div className="px-3 py-3">
+            <div className="flex justify-between items-center">
+              <div className="flex">
+                <div className="flex-shrink-0 h-12 w-12 mr-3">
+                  <img
+                    className="block rounded-full mr-4"
+                    src={`https://api.multiavatar.com/${name}.png`}
+                    alt={name}
+                  />
+                </div>
+                <div>
+                  <h3 className="text-base font-bold capitalize">{name}</h3>
+                  <ul>
+                    {records.map(paid => (
+                      <li key={paid} className="text-sm text-gray-500">
+                        → paid {currency}
+                        {paid}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              <h2 className="text-2xl font-bold">
+                <span className="text-sm mr-1 align-top">$</span>
+                {!records.length
+                  ? 0
+                  : records.reduce((t, paid) => (t += paid), 0)}
+              </h2>
+            </div>
+          </div>
+        </div>
+      ))}
 
-			<Form onSubmit={handleSubmit} />
-		</div>
-	)
+      <Form onSubmit={handleSubmit} />
+    </div>
+  )
 }
